978646834c55faff8a562b94a6447a12f73555a4
Jan Janech <jan.janech@kst.uniza.sk>
2021-01-12 22:54:18 +0100

    tests for explicit member mappings with rules

---
+++ POCOMapper.Test/ExplicitMemberMappingWithNullableObjectRules.cs
@@ -0,0 +1,62 @@
+﻿using KST.POCOMapper.Definition;
+using KST.POCOMapper.Mapping.Decorators;
+using KST.POCOMapper.Mapping.Object;
+using KST.POCOMapper.Mapping.Standard;
+using NUnit.Framework;
+
+namespace KST.POCOMapper.Test
+{
+	class ExplicitMemberMappingWithNullableObjectRules
+	{
+		private class FromInner
+		{
+			public string s;
+		}
+
+		private class From
+		{
+			public FromInner inner;
+		}
+
+		private class ToInner
+		{
+			public string s;
+		}
+
+		private class To
+		{
+			public ToInner inner;
+		}
+
+		private class Mapping : MappingSingleton<Mapping>
+		{
+			private Mapping()
+			{
+				Map<From, To>()
+					.ObjectMappingRules()
+					.Member<FromInner, ToInner>("inner", "inner", def => def
+						.NullableRules()
+					);
+			}
+		}
+
+		[Test]
+		public void MapFromObject()
+		{
+			From from = new From {inner = new FromInner {s = "foo"}};
+
+			To to = Mapping.Instance.Map<From, To>(from);
+			Assert.IsNotNull(to.inner);
+			Assert.AreEqual("foo", to.inner.s);
+		}
+
+		[Test]
+		public void MapFromNull()
+		{
+			From from = new From {inner = null};
+
+			To to = Mapping.Instance.Map<From, To>(from);
+			Assert.IsNull(to.inner);
+		}
+	}
+}
---
+++ POCOMapper.Test/ExplicitMemberMappingWithRules.cs
@@ -0,0 +1,52 @@
+﻿using KST.POCOMapper.Definition;
+using KST.POCOMapper.Mapping.Decorators;
+using KST.POCOMapper.Mapping.Object;
+using KST.POCOMapper.Mapping.Standard;
+using NUnit.Framework;
+
+namespace KST.POCOMapper.Test
+{
+	class ExplicitMemberMappingWithRules
+	{
+		private class From
+		{
+			public string s;
+		}
+
+		private class To
+		{
+			public string s;
+		}
+
+		private class Mapping : MappingSingleton<Mapping>
+		{
+			private Mapping()
+			{
+				Map<From, To>()
+					.ObjectMappingRules()
+					.Member<string, string>("s", "s", def => def
+						.NullableRules()
+						.CopyRules()
+					);
+			}
+		}
+
+		[Test]
+		public void MapFromString()
+		{
+			From from = new From {s = "foo"};
+
+			To to = Mapping.Instance.Map<From, To>(from);
+			Assert.AreEqual("foo", to.s);
+		}
+
+		[Test]
+		public void MapFromNull()
+		{
+			From from = new From {s = null};
+
+			To to = Mapping.Instance.Map<From, To>(from);
+			Assert.IsNull(to.s);
+		}
+	}
+}
